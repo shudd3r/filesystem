e57a59147ecb9621d2408b0a72e5b0446c6eecf0
shudd3r <q3.shudder@gmail.com>
2023-08-05 20:14:59 +0200

    Refactored LocalFile tests

--- tests/Local/LocalFileTest.php
+++ tests/Local/LocalFileTest.php
@@ -12,14 +12,10 @@
 namespace Shudd3r\Filesystem\Tests\Local;
 
 use PHPUnit\Framework\TestCase;
-use Shudd3r\Filesystem\Exception\IOException\UnableToCreate;
-use Shudd3r\Filesystem\Exception\IOException\UnableToReadContents;
-use Shudd3r\Filesystem\Exception\IOException\UnableToRemove;
-use Shudd3r\Filesystem\Exception\IOException\UnableToSetPermissions;
-use Shudd3r\Filesystem\Exception\IOException\UnableToWriteContents;
-use Shudd3r\Filesystem\Generic\ContentStream;
 use Shudd3r\Filesystem\Local\LocalFile;
 use Shudd3r\Filesystem\Local\Pathname;
+use Shudd3r\Filesystem\Generic\ContentStream;
+use Shudd3r\Filesystem\Exception\IOException;
 use Shudd3r\Filesystem\Tests\Fixtures;
 
 require_once dirname(__DIR__) . '/Fixtures/native-override/local.php';
@@ -31,34 +27,35 @@ class LocalFileTest extends TestCase
 
     public function test_exists_for_existing_file_returns_true(): void
     {
-        self::$temp->symlink(self::$temp->file('foo/bar/baz.txt'), 'file.lnk');
-        $this->assertTrue($this->file('foo/bar/baz.txt')->exists());
+        $file = $this->file('foo/bar/baz.txt', 'contents');
+        $this->assertTrue($file->exists());
+        self::$temp->symlink($file->pathname(), 'file.lnk');
         $this->assertTrue($this->file('file.lnk')->exists());
     }
 
     public function test_exists_for_not_existing_file_returns_false(): void
     {
-        self::$temp->symlink(self::$temp->directory('foo/bar/dir'), 'dir.lnk');
         $this->assertFalse($this->file('foo/bar/baz.txt')->exists());
+        $directory = self::$temp->directory('foo/bar/dir');
         $this->assertFalse($this->file('foo/bar/dir')->exists());
+        self::$temp->symlink($directory, 'dir.lnk');
         $this->assertFalse($this->file('dir.lnk')->exists());
     }
 
     public function test_remove_method_deletes_file(): void
     {
-        $path = self::$temp->file('foo/bar.txt');
-        $file = $this->file('foo/bar.txt');
-        $this->assertFileExists($path);
+        $file = $this->file('foo/bar.txt', '');
+        $this->assertFileExists($file->pathname());
         $this->assertTrue($file->exists());
         $file->remove();
-        $this->assertFileDoesNotExist($path);
+        $this->assertFileDoesNotExist($file->pathname());
         $this->assertFalse($file->exists());
     }
 
     public function test_contents_returns_file_contents(): void
     {
-        self::$temp->file('foo.txt', 'contents...');
-        $this->assertSame('contents...', $this->file('foo.txt')->contents());
+        $file = $this->file('foo.txt', $contents = 'contents...');
+        $this->assertSame($contents, $file->contents());
     }
 
     public function test_contents_for_not_existing_file_returns_empty_string(): void
@@ -69,21 +66,16 @@ class LocalFileTest extends TestCase
     public function test_write_for_not_existing_file_creates_file_with_given_contents(): void
     {
         $file = $this->file('foo.txt');
-        $this->assertFalse($file->exists());
-
         $file->write($contents = 'Written contents...');
-        $this->assertSame($contents, file_get_contents(self::$temp->pathname('foo.txt')));
+        $this->assertSame($contents, file_get_contents($file->pathname()));
         $this->assertSame($contents, $file->contents());
     }
 
     public function test_write_for_existing_file_replaces_its_contents(): void
     {
-        $filename = self::$temp->file('foo.txt', 'Original contents...');
-        $file     = $this->file('foo.txt');
-        $this->assertTrue($file->exists());
-
+        $file = $this->file('foo.txt', 'Old contents...');
         $file->write($newContents = 'New contents');
-        $this->assertSame($newContents, file_get_contents($filename));
+        $this->assertSame($newContents, file_get_contents($file->pathname()));
     }
 
     public function test_writeStream_for_not_existing_file_creates_file_with_given_contents(): void
@@ -95,11 +87,9 @@ class LocalFileTest extends TestCase
 
     public function test_writeStream_for_existing_file_replaces_its_contents(): void
     {
-        self::$temp->file('bar.txt', $old = 'old contents');
-        $this->assertSame($old, file_get_contents(self::$temp->pathname('bar.txt')));
-        $stream = new ContentStream($this->resource($new = 'new contents'));
-        $this->file('bar.txt')->writeStream($stream);
-        $this->assertSame($new, file_get_contents(self::$temp->pathname('bar.txt')));
+        $stream = new ContentStream($this->resource($newContents = 'new contents'));
+        $this->file('bar.txt', 'old contents')->writeStream($stream);
+        $this->assertSame($newContents, file_get_contents(self::$temp->pathname('bar.txt')));
     }
 
     public function test_append_to_not_existing_file_creates_file(): void
@@ -113,12 +103,11 @@ class LocalFileTest extends TestCase
 
     public function test_append_to_existing_file_appends_to_existing_contents(): void
     {
-        self::$temp->file('file.txt');
-        $file = $this->file('file.txt');
+        $file = $this->file('file.txt', 'content');
         $file->append('...added');
-        $this->assertSame('...added', $file->contents());
+        $this->assertSame('content...added', $file->contents());
         $file->append(' more');
-        $this->assertSame('...added more', $file->contents());
+        $this->assertSame('content...added more', $file->contents());
     }
 
     public function test_creating_file_with_directory_structure(): void
@@ -138,10 +127,9 @@ class LocalFileTest extends TestCase
 
     public function test_copy_duplicates_contents_of_given_file(): void
     {
-        self::$temp->file('foo.txt', 'Foo contents');
         $file = $this->file('bar.txt');
-        $file->copy($this->file('foo.txt'));
-        $this->assertSame('Foo contents', $file->contents());
+        $file->copy($this->file('foo.txt', $contents = 'Foo contents'));
+        $this->assertSame($contents, $file->contents());
     }
 
     public function test_contentStream_for_not_existing_file_returns_null(): void
@@ -151,8 +139,7 @@ class LocalFileTest extends TestCase
 
     public function test_contentStream_for_existing_file_returns_streamable_contents(): void
     {
-        self::$temp->file('foo.txt', 'foo contents...');
-        $file = $this->file('foo.txt');
+        $file = $this->file('foo.txt', 'foo contents...');
         $this->assertInstanceOf(ContentStream::class, $stream = $file->contentStream());
         $this->assertSame($file->contents(), fread($stream->resource(), 1024));
     }
@@ -162,34 +149,33 @@ class LocalFileTest extends TestCase
         $file  = $this->file('foo/bar/baz.txt');
         $write = fn () => $file->write('something');
 
-        $this->assertIOException(UnableToCreate::class, $write, 'mkdir');
-        $this->assertIOException(UnableToCreate::class, $write, 'file_put_contents');
-        $this->assertIOException(UnableToSetPermissions::class, $write, 'chmod');
-        $this->assertIOException(UnableToWriteContents::class, $write, 'file_put_contents');
+        $this->assertIOException(IOException\UnableToCreate::class, $write, 'mkdir');
+        $this->assertIOException(IOException\UnableToCreate::class, $write, 'file_put_contents');
+        $this->assertIOException(IOException\UnableToSetPermissions::class, $write, 'chmod');
+        $this->assertIOException(IOException\UnableToWriteContents::class, $write, 'file_put_contents');
     }
 
     public function test_runtime_remove_file_failures(): void
     {
-        self::$temp->file('foo/bar/baz.txt', 'contents');
-        $file   = $this->file('foo/bar/baz.txt');
+        $file   = $this->file('foo/bar/baz.txt', 'contents');
         $remove = fn () => $file->remove();
 
-        $this->assertIOException(UnableToRemove::class, $remove, 'unlink');
+        $this->assertIOException(IOException\UnableToRemove::class, $remove, 'unlink');
     }
 
     public function test_runtime_read_file_failures(): void
     {
-        self::$temp->file('foo/bar/baz.txt', 'contents');
-        $file = $this->file('foo/bar/baz.txt');
+        $file = $this->file('foo/bar/baz.txt', 'contents');
         $read = fn () => $file->contents();
 
-        $this->assertIOException(UnableToReadContents::class, $read, 'fopen');
-        $this->assertIOException(UnableToReadContents::class, $read, 'flock');
-        $this->assertIOException(UnableToReadContents::class, $read, 'file_get_contents');
+        $this->assertIOException(IOException\UnableToReadContents::class, $read, 'fopen');
+        $this->assertIOException(IOException\UnableToReadContents::class, $read, 'flock');
+        $this->assertIOException(IOException\UnableToReadContents::class, $read, 'file_get_contents');
     }
 
-    private function file(string $filename): LocalFile
+    private function file(string $filename, string $contents = null): LocalFile
     {
+        if (isset($contents)) { self::$temp->file($filename, $contents); }
         return new LocalFile(Pathname::root(self::$temp->directory())->forChildNode($filename));
     }
 }
