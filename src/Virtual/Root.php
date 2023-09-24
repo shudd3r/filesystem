50613ad1a63be06ac8d3b24d7aacee73754a4f25
shudd3r <q3.shudder@gmail.com>
2023-09-24 23:42:32 +0200

    Changed Root to separate type

---
+++ src/Virtual/Root.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Shudd3r/Filesystem package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Shudd3r\Filesystem\Virtual;
+
+use Shudd3r\Filesystem\Virtual\TreeNode\Directory;
+use Shudd3r\Filesystem\Exception\UnsupportedOperation;
+
+
+class Root
+{
+    private string    $path;
+    private Directory $nodes;
+    private int       $length;
+
+    /**
+     * @param string    $path
+     * @param Directory $nodes
+     */
+    public function __construct(string $path, Directory $nodes = null)
+    {
+        $this->path   = $path;
+        $this->nodes  = $nodes ?? new Directory();
+        $this->length = strlen($path) + (str_ends_with($path, '/') ? 0 : 1);
+    }
+
+    public function node(string $path): TreeNode
+    {
+        return $this->nodes->node($this->relativePath($path));
+    }
+
+    private function relativePath(string $path): string
+    {
+        if (!str_starts_with($path, $this->path)) {
+            throw new UnsupportedOperation();
+        }
+
+        return substr($path, $this->length);
+    }
+}
---
+++ tests/Virtual/RootTest.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of Shudd3r/Filesystem package.
+ *
+ * (c) Shudd3r <q3.shudder@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Shudd3r\Filesystem\Tests\Virtual;
+
+use PHPUnit\Framework\TestCase;
+use Shudd3r\Filesystem\Virtual\Root;
+use Shudd3r\Filesystem\Virtual\TreeNode\Directory;
+use Shudd3r\Filesystem\Virtual\TreeNode\MissingNode;
+use Shudd3r\Filesystem\Exception\UnsupportedOperation;
+
+
+class RootTest extends TestCase
+{
+    public function test_node_method(): void
+    {
+        $root = new Root('vfs://root', $rootDir = new Directory([
+            'foo' => $foo = new Directory([
+                'bar' => $bar = new Directory()
+            ])
+        ]));
+
+        $this->assertSame($rootDir, $root->node('vfs://root'));
+        $this->assertSame($foo, $root->node('vfs://root/foo'));
+        $this->assertSame($bar, $root->node('vfs://root/foo/bar'));
+        $this->assertSame($bar, $root->node('vfs://root/foo')->node('bar'));
+        $this->assertInstanceOf(MissingNode::class, $root->node('vfs://root/foo/bar/baz'));
+    }
+
+    public function test_node_for_not_matching_root_path_throws_exception(): void
+    {
+        $root = new Root('vfs://');
+        $this->expectException(UnsupportedOperation::class);
+        $root->node('virtual://root/path');
+    }
+}
