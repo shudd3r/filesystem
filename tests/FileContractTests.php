3ceb948718d739bdd67d6dad5ff91abb450eeb39
shudd3r <q3.shudder@gmail.com>
2023-11-06 05:40:22 +0100

    Refactored assertions in FileContractTests

--- tests/FileContractTests.php
+++ tests/FileContractTests.php
@@ -33,9 +33,9 @@ trait FileContractTests
 
     public function test_remove_method_deletes_file(): void
     {
-        $file = $this->root(['foo' => ['bar.txt' => '']])->file('foo/bar.txt');
-        $file->remove();
-        $this->assertFalse($file->exists());
+        $root = $this->root(['foo' => ['bar.txt' => '']]);
+        $root->file('foo/bar.txt')->remove();
+        $this->assertSameStructure($root, ['foo' => []]);
     }
 
     public function test_contents_returns_file_contents(): void
@@ -46,111 +46,97 @@ trait FileContractTests
 
     public function test_contents_for_not_existing_file_returns_empty_string(): void
     {
-        $this->assertEmpty($this->root()->file('not-exists.txt')->contents());
+        $this->assertEmpty($this->root([])->file('not-exists.txt')->contents());
     }
 
     public function test_write_for_not_existing_file_creates_file_with_given_contents(): void
     {
-        $file = $this->root()->file('foo.txt');
-        $file->write('Written contents...');
-        $this->assertSame('Written contents...', $file->contents());
+        $root = $this->root([]);
+        $root->file('foo.txt')->write('contents');
+        $this->assertSameStructure($root, ['foo.txt' => 'contents']);
     }
 
     public function test_write_for_existing_file_replaces_its_contents(): void
     {
-        $file = $this->root(['foo.txt' => 'Old contents...'])->file('foo.txt');
-        $file->write('New contents');
-        $this->assertSame('New contents', $file->contents());
+        $root = $this->root(['foo.txt' => 'old']);
+        $root->file('foo.txt')->write('new');
+        $this->assertSameStructure($root, ['foo.txt' => 'new']);
     }
 
     public function test_writeStream_for_not_existing_file_creates_file_with_given_contents(): void
     {
-        $file = $this->root()->file('bar.txt');
-        $file->writeStream($this->stream('foo contents...'));
-        $this->assertSame('foo contents...', $file->contents());
+        $root = $this->root([]);
+        $root->file('foo.txt')->writeStream($this->stream('foo contents...'));
+        $this->assertSameStructure($root, ['foo.txt' => 'foo contents...']);
     }
 
     public function test_writeStream_for_existing_file_replaces_its_contents(): void
     {
-        $file = $this->root(['bar.txt' => 'old contents'])->file('bar.txt');
-        $file->writeStream($this->stream('new contents'));
-        $this->assertSame('new contents', $file->contents());
+        $root = $this->root(['bar.txt' => 'old contents']);
+        $root->file('bar.txt')->writeStream($this->stream('new contents'));
+        $this->assertSameStructure($root, ['bar.txt' => 'new contents']);
     }
 
     public function test_append_to_not_existing_file_creates_file(): void
     {
-        $file = $this->root()->file('file.txt');
-        $this->assertFalse($file->exists());
-        $file->append('contents...');
-        $this->assertTrue($file->exists());
-        $this->assertSame('contents...', $file->contents());
+        $root = $this->root([]);
+        $root->file('file.txt')->append('contents...');
+        $this->assertSameStructure($root, ['file.txt' => 'contents...']);
     }
 
     public function test_append_to_existing_file_appends_to_existing_contents(): void
     {
-        $file = $this->root(['file.txt' => 'content'])->file('file.txt');
+        $root = $this->root(['file.txt' => 'content']);
+        $file = $root->file('file.txt');
         $file->append('...added');
-        $this->assertSame('content...added', $file->contents());
         $file->append(' more');
-        $this->assertSame('content...added more', $file->contents());
+        $this->assertSameStructure($root, ['file.txt' => 'content...added more']);
     }
 
     public function test_creating_file_with_directory_structure(): void
     {
-        $root = $this->root([]);
+        $root = $this->root(['foo' => []]);
         $root->file('foo/bar/baz.txt')->write('contents');
-        $root->file('foo/baz/file.txt')->append('...contents');
-        $expected = ['foo' => ['bar' => ['baz.txt' => 'contents'], 'baz' => ['file.txt' => '...contents']]];
-        $this->assertSameStructure($root, $expected);
+        $root->file('baz/dir/file.txt')->append('...contents');
+        $this->assertSameStructure($root, [
+            'foo' => ['bar' => ['baz.txt' => 'contents']],
+            'baz' => ['dir' => ['file.txt' => '...contents']]
+        ]);
     }
 
     public function test_copy_duplicates_contents_of_given_file(): void
     {
         $root = $this->root(['foo.txt' => 'Foo contents']);
-        $file = $root->file('bar.txt');
-        $file->copy($root->file('foo.txt'));
-        $this->assertSame('Foo contents', $file->contents());
+        $root->file('bar.txt')->copy($root->file('foo.txt'));
+        $this->assertSameStructure($root, ['foo.txt' => 'Foo contents', 'bar.txt' => 'Foo contents']);
     }
 
     public function test_moveTo_moves_file_to_given_directory(): void
     {
-        $root   = $this->root(['foo' => ['file.txt' => 'foo contents...']]);
-        $file   = $root->file('foo/file.txt');
-        $target = $root->file('bar/file.txt');
-        $file->moveTo($root->subdirectory('bar'));
-        $this->assertSame('foo contents...', $target->contents());
-        $this->assertFalse($file->exists());
+        $root = $this->root(['foo' => ['file.txt' => 'foo contents...']]);
+        $root->file('foo/file.txt')->moveTo($root->subdirectory('bar'));
+        $this->assertSameStructure($root, ['foo' => [], 'bar' => ['file.txt' => 'foo contents...']]);
     }
 
     public function test_moveTo_with_specified_name_moves_file_with_changed_name(): void
     {
-        $root   = $this->root(['baz.txt' => 'baz contents...']);
-        $file   = $root->file('baz.txt');
-        $target = $root->file('foo/bar/moved.file');
-        $file->moveTo($root, 'foo/bar/moved.file');
-        $this->assertSame('baz contents...', $target->contents());
-        $this->assertFalse($file->exists());
+        $root = $this->root(['baz.txt' => 'baz contents...']);
+        $root->file('baz.txt')->moveTo($root, 'foo/bar/moved.file');
+        $this->assertSameStructure($root, ['foo' => ['bar' => ['moved.file' => 'baz contents...']]]);
     }
 
     public function test_moveTo_overwrites_existing_file(): void
     {
-        $root   = $this->root(['foo' => ['bar' => ['baz.txt' => 'old contents']], 'foo.txt' => 'new contents']);
-        $file   = $root->file('foo.txt');
-        $target = $root->file('foo/bar/baz.txt');
-        $this->assertSame('old contents', $target->contents());
-        $file->moveTo($root->subdirectory('foo'), 'bar/baz.txt');
-        $this->assertSame('new contents', $target->contents());
-        $this->assertFalse($file->exists());
+        $root = $this->root(['foo' => ['bar' => ['baz.txt' => 'old contents']], 'foo.txt' => 'new contents']);
+        $root->file('foo.txt')->moveTo($root->subdirectory('foo'), 'bar/baz.txt');
+        $this->assertSameStructure($root, ['foo' => ['bar' => ['baz.txt' => 'new contents']]]);
     }
 
-    public function test_moveTo_for_not_existing_file_has_no_effect(): void
+    public function test_moveTo_for_not_existing_file_is_ignored(): void
     {
-        $root   = $this->root(['bar.txt' => 'old contents']);
-        $file   = $root->file('foo/bar.txt');
-        $target = $root->file('bar.txt');
-        $file->moveTo($root);
-        $this->assertSame('old contents', $target->contents());
-        $this->assertFalse($file->exists());
+        $root = $this->root(['foo' => []]);
+        $root->file('bar.txt')->moveTo($root->subdirectory('foo'));
+        $this->assertSameStructure($root, ['foo' => []]);
     }
 
     public function test_method_calls_on_invalid_file_path_throw_exception(): void
