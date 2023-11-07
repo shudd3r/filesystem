bf9ec0a4a79c1fd316cbe50ab08bd057189394a4
shudd3r <q3.shudder@gmail.com>
2023-11-07 02:55:59 +0100

    Refactored LocalFileTest methods

--- tests/Local/LocalFileTest.php
+++ tests/Local/LocalFileTest.php
@@ -12,7 +12,7 @@
 namespace Shudd3r\Filesystem\Tests\Local;
 
 use Shudd3r\Filesystem\Tests\FileContractTests;
-use Shudd3r\Filesystem\Exception;
+use Shudd3r\Filesystem\Exception\IOException;
 
 
 class LocalFileTest extends LocalFilesystemTests
@@ -21,39 +21,38 @@ class LocalFileTest extends LocalFilesystemTests
 
     public function test_contentStream_for_not_existing_file_returns_null(): void
     {
-        $this->assertNull($this->root()->file('foo.txt')->contentStream());
+        $this->assertNull($this->root([])->file('foo.txt')->contentStream());
     }
 
     public function test_contentStream_for_existing_file_returns_streamable_contents(): void
     {
         $file = $this->root(['foo.txt' => 'foo contents...'])->file('foo.txt');
-        $this->assertSame($file->contents(), $file->contentStream()->contents());
+        $this->assertSame('foo contents...', $file->contentStream()->contents());
     }
 
     public function test_runtime_file_write_failures(): void
     {
         $file  = $this->root([])->file('foo/bar/baz.txt');
         $write = fn () => $file->write('something');
-        $this->assertIOException(Exception\IOException\UnableToCreate::class, $write, 'mkdir');
-        $this->assertIOException(Exception\IOException\UnableToCreate::class, $write, 'file_put_contents');
-        $this->assertIOException(Exception\IOException\UnableToSetPermissions::class, $write, 'chmod');
-        $this->assertIOException(Exception\IOException\UnableToWriteContents::class, $write, 'file_put_contents');
+        $this->assertIOException(IOException\UnableToCreate::class, $write, 'mkdir');
+        $this->assertIOException(IOException\UnableToCreate::class, $write, 'file_put_contents');
+        $this->assertIOException(IOException\UnableToSetPermissions::class, $write, 'chmod');
+        $this->assertIOException(IOException\UnableToWriteContents::class, $write, 'file_put_contents');
     }
 
     public function test_runtime_remove_file_failures(): void
     {
         $file   = $this->root(['foo' => ['bar.txt' => 'contents']])->file('foo/bar.txt');
         $remove = fn () => $file->remove();
-        $this->assertIOException(Exception\IOException\UnableToRemove::class, $remove, 'unlink');
+        $this->assertIOException(IOException\UnableToRemove::class, $remove, 'unlink');
     }
 
     public function test_runtime_read_file_failures(): void
     {
-        $file      = $this->root(['foo' => ['bar.txt' => 'contents']])->file('foo/bar.txt');
-        $read      = fn () => $file->contents();
-        $exception = Exception\IOException\UnableToReadContents::class;
-        $this->assertIOException($exception, $read, 'fopen');
-        $this->assertIOException($exception, $read, 'flock');
-        $this->assertIOException($exception, $read, 'file_get_contents');
+        $file = $this->root(['foo' => ['bar.txt' => 'contents']])->file('foo/bar.txt');
+        $read = fn () => $file->contents();
+        $this->assertIOException(IOException\UnableToReadContents::class, $read, 'fopen');
+        $this->assertIOException(IOException\UnableToReadContents::class, $read, 'flock');
+        $this->assertIOException(IOException\UnableToReadContents::class, $read, 'file_get_contents');
     }
 }
